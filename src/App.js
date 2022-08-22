f19a35758e0e559edf94b5240d18b745548805b7
coachemmy <coachemmyb@gmail.com>
2022-08-22 04:03:54 +0100

    commit them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.scss";
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleSubstract = () => {
+    setCount (count -1);
+  }
+
+  const handleReset = () => {
+    setCount(0);
+  }
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  }
+
+  let color = 'white'
+
+  if (count >=1){
+    color = 'yellow'
+  } else if (count < 0){
+    color = 'red'
+  } else{
+    color = 'white'
+  }
+
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <section className = 'counter-sec --flex-center'>
+      <div className  ='container counter --card --center-all'>
+        <h1 className = '--text-light'>EmmyB Counter App</h1>
+        <p className = 'count --my2 --text-md --text-light --fw-bold'  style={{color:color}}> {count} </p>
+        <div className = 'buttons'>
+          <button className = '--btn --btn-danger' onClick ={handleSubstract}> -Subtract</button>
+          <button className = '--btn' onClick ={handleReset}> !Reset</button>
+          <button className = '--btn --btn-success' onClick ={handleAdd}> +Add</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
